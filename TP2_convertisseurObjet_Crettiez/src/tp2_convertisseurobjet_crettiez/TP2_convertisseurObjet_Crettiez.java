c3720617d16f7d81722743e974c16a5edbb31d1e
benja <benja@DESKTOP-SR3KDU6.education.lakanal.epf.fr>
2022-10-25 09:40:31 +0200

    TP2 Exercice 1 terminé     Exercice 2 part 3 terminée

---
+++ TP2_convertisseurObjet_Crettiez/src/tp2_convertisseurobjet_crettiez/TP2_convertisseurObjet_Crettiez.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_crettiez;
+
+/**
+ *
+ * @author benja
+ */
+public class TP2_convertisseurObjet_Crettiez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Convertisseur masse = new Convertisseur ();
+        System.out.println(masse);
+        
+    }
+    
+    double temp;
+    temp=0;
+    double ind;
+    ind=0;
+    Scanner sc;
+    sc = new Scanner(System.in);
+    System.out.println("Bonjour, saisissez une valeur :");
+    temp= sc.nextDouble();
+    System.out.println("Saisissez la conversion que vous souhaitez effectuer: \n1) De Kelvin vers Celsius 
+    
+}
